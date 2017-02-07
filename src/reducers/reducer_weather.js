0c3ff681cd42e5f1965280f7aecdb6d3636650c3
alexkarevoll <alex.karevoll@gmail.com>
2017-02-07 11:37:38 -0800

    return new state in weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,15 @@
-export default function (state=null, action) {
+import {FETCH_WEATHER} from '../actions/index'
+
+export default function (state=[], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return a brand new state array by concating the
+      // existing data with the new data.
+      // DONT MUTATE STATE. ALWAYS RETURN A NEW STATE. dont use state.push()
+      // return state.concat([action.payload.data]) //new entry at the end
+      return [action.payload.data, ...state] //new entry at the beginning
+    default:
+
+  }
   return state
 }
